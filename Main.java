7a1d55e5096825361ebc4ef8b858bdd3af7d8221
Hector Diaz <3kthor3adward@gmail.com>
2020-08-28 17:56:12 -0500

    code clean up & readme improvements

--- Main.java
+++ Main.java
@@ -6,6 +6,8 @@ import java.security.PublicKey;
 import java.security.spec.MGF1ParameterSpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.OAEPParameterSpec;
@@ -13,28 +15,29 @@ import javax.crypto.spec.PSource;
 
 public class Main {
 
-    private static String encrypt(String key, String str) throws Exception {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    private static String encrypt(String keyPath, String str) throws Exception {
         PublicKey pk = KeyFactory
             .getInstance("RSA")
             .generatePublic(new X509EncodedKeySpec(Base64
                 .getDecoder()
-                .decode(Files
-                    .readString(Paths.get(key))
+                .decode(
+                    //Files.readString(Paths.get(key)) //Java 11+
+                    (new String(Files.readAllBytes(Paths.get(keyPath))))
                     .replace("-----BEGIN PUBLIC KEY-----\n", "")
                     .replaceAll(System.lineSeparator(), "")
                     .replace("-----END PUBLIC KEY-----", ""))));
 
         //https://cloud.google.com/kms/docs/encrypt-decrypt-rsa#encrypt_data
         Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
-        OAEPParameterSpec oaepParams =
-            new OAEPParameterSpec(
-                "SHA-256", "MGF1", MGF1ParameterSpec.SHA256, PSource.PSpecified.DEFAULT);
-        cipher.init(Cipher.ENCRYPT_MODE, pk, oaepParams);
+        cipher.init(Cipher.ENCRYPT_MODE, pk, new OAEPParameterSpec("SHA-256", "MGF1", MGF1ParameterSpec.SHA256, PSource.PSpecified.DEFAULT));
 
         return Base64.getEncoder().encodeToString(cipher.doFinal(str.getBytes(StandardCharsets.UTF_8)));
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println(String.format("Encrypt %s with %s\n\n%s", args[1], args[0], encrypt(args[0], args[1])));
+        String result = encrypt(args[0], args[1]);
+        LOGGER.log(Level.INFO, "{0}", result);
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-Encryption examples for keys generated on GCP KMS.
+Encryption example for asymmetric keys generated with GCP KMS.
+
+**Requires Java 8+**
+
+1. Create an asymmetric key with GCP's [KMS](https://cloud.google.com/kms/docs/creating-asymmetric-keys#kms-create-key-ring-web)
+2. Download the public key using the [Web-UI](https://cloud.google.com/kms/docs/retrieve-public-key#kms-get-public-key-web)
+3. Save it as **key.pub** in the same folder as the **Main.java** file.
+4. Run ```javac Main.java && java Main "key.pub" "text"```
+
+Sample output:
+```
+Aug 28, 2020 5:49:39 PM Main main
+INFO: sfNT3IMcGFUDQBnMbRn2I6HsjyUptOooJGyHubtm65WcQA18lW16Br...
+```
+
+5. Follow [the instructions](https://cloud.google.com/kms/docs/encrypt-decrypt-rsa#kms-decrypt-asymmetric-cli) to decrypt the text.
